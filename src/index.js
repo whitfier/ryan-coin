38abbe945a29e6b1c8189b1ce4a96f5be208e136
Whitfield <whitryan@amazon.com>
2018-06-18 04:06:46 -0700

    Add normalize css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Header from "./components/common/Header";
 import List from "./components/list/List";
 import Detail from "./components/detail/Detail";
 import NotFound from "./components/notfound/NotFound";
+import "normalize.css";
 import "./index.css";
 
 const App = () => {
